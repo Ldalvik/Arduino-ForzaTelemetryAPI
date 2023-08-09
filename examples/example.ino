924ae2368c06851cb2f4bf334412d468d5a090cc
Ldalvik <subaruforge@gmail.com>
2023-08-09 14:21:39 -0400

    Add example code

--- examples/example.ino
+++ examples/example.ino
@@ -4,7 +4,7 @@
 const char *SSID = "YOUR_WIFI";
 const char *PASSWORD = "YOUR_PASSWORD";
 
-ForzaAPI forza();
+ForzaAPI forza = ForzaAPI();
 
 void setup() {
   Serial.begin(115200);
@@ -37,20 +37,54 @@ void setup() {
  * OnDataReceived - this will be called whenever data is received. This is usually
  * once every 33.33ms, or 30 frames per second. Data will not be received here if the game is paused.
  */
-void onDataReceived(Telemetry *telemetry) {
+void onDataReceived(Telemetry telemetry) {
+    // Access data directly from the data packet
   Serial.print("Speed (meters/s): ");
-  Serial.println(telemetry->Speed);
-  Serial.print("Speed (meters/s): ");
-  Serial.println(telemetry->Speed);
+  Serial.print(telemetry.Speed);
+  Serial.print("    -    ");
+
+  // Access data via API helper functions 
+  Serial.print("Speed (meters/s): "); 
+  Serial.print(forza.getSpeed_MetersPerSecond());   
+  Serial.print("    -    ");
+
+  // Access data via API with helper functions
+  Serial.print("Speed (mph): ");
+  Serial.print(forza.getSpeed_MilesPerHour());
+  Serial.print("    -    ");
+  Serial.print("Speed (kph): ");
+  Serial.println(forza.getSpeed_KilometersPerHour());
 }
 
 /*
  * OnCarChanged - this will be called ONCE when the player switches cars (tracked by car ordinal).
  * All relevant data is under the "Car" struct, otherwise use the helper methods which may return more descriptive data.
  */
-void onCarChanged(Car car, short ordinal) {
-  Serial.println("---- New car ---- ");
-  Serial.print("Ordinal: "); Serial.println(ordinal);
+void onCarChanged(Car car) {
+   Serial.println("---- New car ---- ");
+  Serial.print("Ordinal: ");
+  Serial.println(car.Ordinal);
+
+  // Get text version of car class
+  Serial.print("Class: ");
+  Serial.println(forza.getCarClass());
+  // Serial.print("Class: "); Serial.println(car.Class);
+
+  // Get text version of drivetrain type
+  Serial.print("Drivetrain type: ");
+  Serial.println(forza.getCarDrivetrainType());
+  // Serial.print("Drivetrain type: "); Serial.println(car.DrivetrainType);
+
+  Serial.print("# of cyclinders: ");
+  Serial.println(car.NumCylinders);
+
+  // Get text version of car type
+  Serial.print("Type: ");
+  Serial.println(forza.getCarType());
+  // Serial.print("Type: "); Serial.println(car.Type);
+
+  Serial.print("Performance Index (PI): ");
+  Serial.println(car.PerformanceIndex);
 }
 
 /* 
@@ -59,18 +93,18 @@ void onCarChanged(Car car, short ordinal) {
  * updates to OnDataReceived while the game is paused, remove line 58 from 'ForzaTelemetryAPI.cpp'.
  * Please note that the only data that is updated while the game is paused is the Timestamp.
 */
-void onGamePaused(Telemetry *telemetry) {
+void onGamePaused(Telemetry telemetry) {
   Serial.print("Game paused at ");
-  Serial.println(telemetry->TimestampMS);
+  Serial.println(telemetry.TimestampMS);
 }
 
 /* 
  * OnGameUnpaused - this will be called ONCE when the player unpauses the game. Data will begin to be 
  * received in OnDataReceived.
  */
-void onGameUnpaused(Telemetry *telemetry) {
+void onGameUnpaused(Telemetry telemetry) {
   Serial.print("Game unpaused at ");
-  Serial.println(telemetry->TimestampMS);
+  Serial.println(telemetry.TimestampMS);
 }
 
 // Update event listeners 
