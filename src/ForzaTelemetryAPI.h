8370912133790e9f49527adaf7fa96f34dfa69c4
Ldalvik <subaruforge@gmail.com>
2023-08-09 12:27:44 -0400

    Fixed dangling pointer bug

--- src/ForzaTelemetryAPI.h
+++ src/ForzaTelemetryAPI.h
@@ -9,10 +9,10 @@ class ForzaAPI
 public:
     const int PACKET_SIZE = 324;
 
-    typedef void (*OnDataReceived)(Telemetry *telemetryData);
-    typedef void (*OnCarChanged)(Car car, short ordinal);
-    typedef void (*OnGamePaused)(Telemetry *telemetryData);
-    typedef void (*OnGameUnpaused)(Telemetry *telemetryData);
+    typedef void (*OnDataReceived)(Telemetry telemetryData);
+    typedef void (*OnCarChanged)(Car car);
+    typedef void (*OnGamePaused)(Telemetry telemetryData);
+    typedef void (*OnGameUnpaused)(Telemetry telemetryData);
 
     ForzaAPI(const int port = 5300);
     void connect();
@@ -37,33 +37,12 @@ public:
     float getPower_Watts();
     float getPower_Horsepower();
 
-    // float getBoost();
-    // float getFuel();
-
-    // int getRaceDistanceTraveled();
-    // int getRaceBestLap();
-    // int getRaceLastLap();
-    // int getRaceCurrentLap();
-    // int getRaceCurrentTime();
-    // int getRaceLapNumber();
-    // int getRacePosition();
-
-    // int getAccel();
-    // int getBrake();
-    // int getClutch();
-    // int getHandBrake();
-    // int getGear();
-    // int getSteer();
-
-    // int getNormalizedDrivingLine();
-    // int getNormalizedAIBrakeDifference();
-
 private:
     int port;                  // Assigned UDP port. default 5300
     bool isPaused = false;     // Track last game state
-    short lastOrdinal = 0; // Track last car ordinal
+    short lastOrdinal = 0;     // Track last car ordinal
     WiFiUDP UDP;               // UDP connection class
-    Telemetry *telemetryData;  // Telemetry data schema
+    Telemetry telemetryData;   // Telemetry data schema
 };
 
 #endif
